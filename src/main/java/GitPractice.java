b90087726ebd9946a7d930f857352b133df0260a
mertyarsoy <mertyarsoy@gmail.com>
2023-04-14 19:28:41 -0400

    add GitPractice

---
+++ src/main/java/GitPractice.java
@@ -0,0 +1,6 @@
+public class GitPractice {
+    public static void main(String[] args) {
+        System.out.println("Git Practice");
+        System.out.println(" Git Practice1 command ");
+    }
+}
